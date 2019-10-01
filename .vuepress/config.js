f4a2005603634d1160b7d0599a25f332f91c0d0e
Matteo Moschitta <info@matteomoschitta.it>
2019-10-01 02:02:56 +0200

    [MM] add navbar

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'LaraMong',
+    description: 'Meta Description',
+    themeConfig: {
+        nav: [
+            { text: 'Guide', link: '/guide/' },
+            { text: 'Github', link: 'https://github.com/offline-agency/laravel-mongo-auto-sync' },
+        ]
+    }
+}
