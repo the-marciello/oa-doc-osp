faaed2505419aa740ed6a3d3dae5cc1ebe89b73d
Matteo Moschitta <info@matteomoschitta.it>
2019-10-07 17:04:32 +0200

    [MM] remove & add files

---
+++ .vuepress/theme/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+        © {{ new Date().getFullYear() }}
+        <a href="/">Offline Agency</a> |
+        <a :href="gitUrl" target="_blank" >Github</a>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        computed: {
+            data() {
+                return this.$page.frontmatter
+            },
+            gitUrl () {
+                if (this.$page.frontmatter.gitName) {
+                    return 'https://github.com/offline-agency/' + this.$page.frontmatter.gitName
+                }
+                else {
+                    return 'https://github.com/offline-agency/'
+                }
+            }
+        }
+    }
+
+</script>
+
+<style lang="stylus">
+
+    .footer {
+        padding: 2.5rem
+        border-top: 1px solid $borderColor
+        text-align: center
+        color: lighten($textColor, 25%)
+    }
+
+</style>
