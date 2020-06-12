139c76ecd17eeccde414de743856d8c27d5779ab
Matteo Moschitta <matteo@offlineagency.it>
2020-06-12 09:33:37 +0200

    [MM] add reference from plugin -> vuepress-plugin-container

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -30,5 +30,31 @@ module.exports = {
                 notFoundPath: '/404.html',
             },
         ],
+        [
+            'vuepress-plugin-container',
+            {
+                type: 'right',
+                defaultTitle: '',
+            },
+        ],
+        [
+            'vuepress-plugin-container',
+            {
+                type: 'theorem',
+                before: info => `<div class="theorem"><p class="title">${info}</p>`,
+                after: '</div>',
+            },
+        ],
+        // this is how VuePress Default Theme use this plugin
+        [
+            'vuepress-plugin-container',
+            {
+                type: 'tip',
+                defaultTitle: {
+                    '/': 'TIP',
+                    '/zh/': '提示',
+                },
+            },
+        ],
     ],
     };
